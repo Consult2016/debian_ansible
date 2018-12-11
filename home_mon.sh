fc3c24edc467260f0a2b6df4309e7ceaab3fd5e4
Joseph <jbarcia99@yahoo.com>
2018-12-11 10:36:31 -0500

    fixes

---
+++ home_mon.sh
@@ -0,0 +1,3 @@
+xrandr --output DP-2-1 --mode 2560x1440 --pos 0x0
+xrandr --output DP-2-3 --mode 2560x1440 --pos 2560x0 #--right-of DP-2-1
+xrandr --output DP-1 --mode 1280x1024 --pos 5120x416 #--right-of DP-2-3
