20ac8a3a9de74d058901afbf657749cd89090564
Joseph <jbarcia99@yahoo.com>
2019-01-14 13:21:11 -0500

    updated custom mointors setup

--- home_mon.sh
+++ home_mon.sh
@@ -1,3 +1,4 @@
 xrandr --output DP-2-1 --mode 2560x1440 --pos 0x0
 xrandr --output DP-2-3 --mode 2560x1440 --pos 2560x0 #--right-of DP-2-1
 xrandr --output DP-1 --mode 1280x1024 --pos 5120x416 #--right-of DP-2-3
+~/.config/bspwm/multi-monitor.sh
--- laptop_mon.sh
+++ laptop_mon.sh
@@ -1,1 +1,2 @@
 xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --primary
+~/.config/bspwm/multi-monitor.sh
